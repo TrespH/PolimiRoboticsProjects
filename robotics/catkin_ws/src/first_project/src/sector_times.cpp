5ba74df2f61853b2368a61e8bfe6b094279af214
Piero Burigana <pieroburigana@MacBookAir.Home>
2025-04-29 21:13:22 +0200

    sector_times corrections

--- robotics/catkin_ws/src/first_project/src/sector_times.cpp
+++ robotics/catkin_ws/src/first_project/src/sector_times.cpp
@@ -13,7 +13,7 @@ private:
     ros::Subscriber gps_pose_sub;
     ros::Publisher sector_times_pub;
     std::vector<std::pair<double,double>> checkpoints = {
-        {45.618928, 9.281170},  // Start/Finish
+        {45.618928, 9.281170},  // Checkpoint 0 (ARRIVAL)
         {45.630136, 9.290665},  // Checkpoint 1
         {45.622962, 9.286304}   // Checkpoint 2
     };
@@ -25,7 +25,7 @@ private:
     double speedSum;
     double mean_speed;
     double sectorTime;
-    const double R = 6371000.0;
+    const double R = 6371000.0; //heart radius
     bool is_started;
 
 public:
@@ -37,30 +37,39 @@ public:
     }
 
     void speedCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
-        double speed = msg->point.y;
+        double speed = msg->point.y; //speed in km/h
 
-        if (is_started){
-            speedSum += speed;
-            speedCount++;
-        } else if (speed > 2){ //start time when the car starts moving
-            speedSum = speed;
-            speedCount = 1;
-            is_started = true;   
-            startTime = msg->header.stamp;
+        if (is_started && msg->header.stamp < startTime) {   //to launch again rosbag without re-launching roscore
+            ROS_WARN("Detected rosbag re-launch ---> data reset)");
+            reset();
         }
 
-        ROS_INFO("Sector %d: Time=%.2fs, Speed=%.2f km/h", currentSector, sectorTime, speed); //data print for every speedsteer callback
+        if (is_started){ 
+            speedSum += speed;
+            speedCount++;
+            now = msg->header.stamp;
+            sectorTime = (now - startTime).toSec();
+        } else { //first topic
+            startTime = msg->header.stamp;   
+            speedSum = speed;
+            speedCount = 1;
+            is_started = true;    
+            ROS_INFO("START!");    
+        }
+
+        mean_speed = (speedCount > 0) ? (speedSum / speedCount) : 0.0;  //to avoid errors
+        sector_msg.current_sector = currentSector;
+        sector_msg.current_sector_time = sectorTime;
+        sector_msg.current_sector_mean_speed = mean_speed;
+        sector_times_pub.publish(sector_msg);  //message post in speedCallback because speedsteer is the most frequent subscription
     }
 
     void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
         double curr_lat = msg->latitude;
         double curr_lon = msg->longitude;
-        //ros::Time now = msg->header.stamp;
 
         if (is_started){
             checkSectorTransition(curr_lat, curr_lon);
-            now = msg->header.stamp;
-            sectorTime = (now - startTime).toSec();
         }
     }
 
@@ -68,32 +77,39 @@ public:
         int target_checkpoint = currentSector % 3; //checkpoint that follows the current sector (1-->1, 2-->2, 3-->0)
         double target_lat = checkpoints[target_checkpoint].first; //checkpoint latitude
         double target_lon = checkpoints[target_checkpoint].second; //checkpoint longitude
-        double dist_to_cp = calculateDistance (curr_lat, curr_lon, target_lat, target_lon);
+        double dist_to_cp = calculateDistance (curr_lat, curr_lon, target_lat, target_lon); 
 
         if (dist_to_cp < 5){   //if I'm in the neighborhood of the checkpoint        
-            mean_speed = (speedCount > 0) ? (speedSum / speedCount) : 0.0; 
 
-            sector_msg.current_sector = currentSector;
-            sector_msg.current_sector_time = sectorTime;
-            sector_msg.current_sector_mean_speed = mean_speed;
-            sector_times_pub.publish(sector_msg);
+            ROS_INFO("Sector %d: Time=%.2fs, Speed=%.2f km/h", currentSector, sectorTime, mean_speed); //data print
 
-            currentSector = (currentSector % 3) + 1; //sector switching
             startTime = now;
+            currentSector = (currentSector % 3) + 1; //sector switching
             speedSum = 0.0;
             speedCount = 0;
 
-            ROS_INFO("Entering in the sector number %d", currentSector);
+            if (currentSector == 1){
+                ROS_INFO("FINISH!"); //the transition from sector 3 to sector 1 corresponds to the arrival
+            }
         }
     }
 
-    double calculateDistance (double lat1, double lon1, double lat2, double lon2){ //to convert from gps coordinates to metres we need to use Haversine formula
+    double calculateDistance (double lat1, double lon1, double lat2, double lon2){ //Haversine formula (from coordinates to meters)
         double dlat = (lat1 - lat2) * M_PI / 180; //delta lat in radiant
         double dlon = (lon1 - lon2) * M_PI / 180; //delta lon in radiant
         double a = sin(dlat/2)*sin(dlat/2)+cos(lat1*M_PI/180)*cos(lat2*M_PI/180)*sin(dlon/2)*sin(dlon/2);
         double b = std::asin(sqrt(a));
         double dist = 2 * R * b;
         return dist;
+    }
+
+    void reset() {
+        speedSum = 0.0;
+        speedCount = 0;
+        mean_speed = 0.0;
+        sectorTime = 0.0;
+        currentSector = 1;
+        is_started = false;
     }
 };
 
